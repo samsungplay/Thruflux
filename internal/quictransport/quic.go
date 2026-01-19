37dcdbaafe945a953a4bba87eec6dc08b71f21ac
parkmin <samsungplayee@gmail.com>
2026-01-19 20:20:18 +0900

    Text

--- internal/quictransport/quic.go
+++ internal/quictransport/quic.go
@@ -7,9 +7,11 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"crypto/x509/pkix"
+	"fmt"
 	"log/slog"
 	"math/big"
 	"net"
+	"os"
 	"time"
 
 	"github.com/quic-go/quic-go"
@@ -112,19 +114,21 @@ func Listen(ctx context.Context, udpConn net.PacketConn, logger *slog.Logger) (*
 
 // ListenWithConfig creates a QUIC listener on the given PacketConn using a custom config.
 func ListenWithConfig(ctx context.Context, udpConn net.PacketConn, logger *slog.Logger, config *quic.Config) (*quic.Listener, error) {
+	fmt.Fprintf(os.Stderr, "[TRACE quic] ListenWithConfig called, localAddr=%v\n", udpConn.LocalAddr())
 	tlsConfig := ServerConfig()
 	if config == nil {
 		config = DefaultServerQUICConfig()
 	}
 
-	logger.Info("QUIC listen starting", "local_addr", udpConn.LocalAddr())
-
+	fmt.Fprintf(os.Stderr, "[TRACE quic] calling quic.Listen...\n")
 	listener, err := quic.Listen(udpConn, tlsConfig, config)
 	if err != nil {
+		fmt.Fprintf(os.Stderr, "[TRACE quic] quic.Listen FAILED: %v\n", err)
 		logger.Error("QUIC listen failed", "error", err, "local_addr", udpConn.LocalAddr())
 		return nil, err
 	}
 
+	fmt.Fprintf(os.Stderr, "[TRACE quic] quic.Listen returned OK\n")
 	logger.Info("QUIC listener created", "local_addr", udpConn.LocalAddr())
 	return listener, nil
 }
