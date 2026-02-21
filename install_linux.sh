0bb38c73c415f39ce28992b416c39477c17f0086
parkmin <samsungplayee@gmail.com>
2026-02-21 13:02:36 -0600

    Update install scripts

--- install_linux.sh
+++ install_linux.sh
@@ -5,7 +5,7 @@ APP="thru"
 INSTALL_DIR="/usr/local/bin"
 TARGET="${INSTALL_DIR}/${APP}"
 UDP_BYTES=$((16 * 1024 * 1024))
-URL_LINUX_X64="https://github.com/samsungplay/Thruflux-C-/releases/download/v18/thru_linux"
+URL_LINUX_X64="https://github.com/samsungplay/Thruflux/releases/download/Beta/thru_linux"
 
 log()  { printf "%s\n" "$*"; }
 warn() { printf "warning: %s\n" "$*" >&2; }
