425214bcb1e1f75ab5f1c5ccfade99bb5f503b43
parkmin <samsungplayee@gmail.com>
2026-03-04 13:35:47 -0600

    Fix resume / open file not working by ensuring to open files in appropriate modes in FileHandle

--- common/Contexts.hpp
+++ common/Contexts.hpp
@@ -70,7 +70,7 @@ namespace common {
             paths[id] = std::move(path);
         }
 
-        llfio::file_handle* acquire(uint32_t id, bool write = false) {
+        llfio::file_handle* acquire(uint32_t id, bool write = false, bool append = false) {
             if (id >= entries.size() || id >= paths.size() || paths[id].empty()) return nullptr;
 
             Entry &e = entries[id];
@@ -92,7 +92,7 @@ namespace common {
 
             const auto native = paths[id].native();
 
-            auto opened = write ? llfio::file({},llfio::path_view(native), llfio::file_handle::mode::write, llfio::file_handle::creation::truncate_existing) : llfio::file({},
+            auto opened = write ? llfio::file({},llfio::path_view(native), llfio::file_handle::mode::write, append ? llfio::file_handle::creation::if_needed : llfio::file_handle::creation::always_new) : llfio::file({},
                 llfio::path_view(native));
             if (!opened) {
                 spdlog::error("Failed to open file id {} path='{}' err={}", id, paths[id].string(), opened.error().message());
