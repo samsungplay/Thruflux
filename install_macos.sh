1bbe6a3b8b2187be816169875384e51618ccd3ed
parkmin <samsungplayee@gmail.com>
2026-03-04 22:21:12 -0600

    Update installer scripts

--- install_macos.sh
+++ install_macos.sh
@@ -6,7 +6,7 @@ INSTALL_DIR="/usr/local/bin"
 TARGET="${INSTALL_DIR}/${APP}"
 UDP_BYTES=$((16 * 1024 * 1024))
 
-URL_MAC="https://github.com/samsungplay/Thruflux/releases/download/v0.3.1/thru_mac"
+URL_MAC="https://github.com/samsungplay/Thruflux/raw/refs/heads/main/frontend/binaries/macos/thru_mac"
 PLIST="/Library/LaunchDaemons/com.thruflux.udptune.plist"
 LABEL="com.thruflux.udptune"
 
